3253fd45ec050f15c8032c51d7dd2f4f1b894743
SarenkGD <sarenka@mojasiec.com>
2016-04-10 13:18:44 +0200

    Updated to new LMS version

--- stock/accesstable.php
+++ stock/accesstable.php
@@ -1,7 +1,17 @@
 <?php
-$access['table'][100]['name']      = 'magazyn';
+$access_table['lms-stck-full'] = array(
+	'label' => trans('full access to lms-stck module'),
+	'allow_regexp' => '^(stck)[a-z]+$'
+	);
+
+$access_table['lms-stck-reports'] = array(
+	'label' => trans('full access lms-stck reports'),
+	'allow_regexp' => '^printstock$'
+	);
+
+/*$access['table'][100]['name']      = 'magazyn';
 $access['table'][100]['allow_reg'] = '^(stck)[a-z]+$';
 
 $access['table'][101]['name']           = 'raporty magazynowe';
 $access['table'][101]['allow_reg']      = '^printstock$';
-?>
+*/?>
